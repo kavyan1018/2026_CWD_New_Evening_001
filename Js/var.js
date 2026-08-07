95b44fd94f1900ecd8ccad8b5c8658613e924a6e
Kavyan Nayak <kavyannayak104@gmail.com>
2026-08-07 18:25:01 +0530

    varibales

---
+++ Js/var.js
@@ -0,0 +1,10 @@
+var a = 10;
+var b = 20;
+
+var sum = a + b;
+var diff = a - b;
+var product = a * b;
+
+console.log("The Addition of 2 numbers :", sum);
+console.log("The Subtraction of 2 numbers :", diff);
+console.log("The Multiplication of 2 numbers :", product);
