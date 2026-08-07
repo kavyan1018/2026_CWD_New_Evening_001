0e79db5fab14f3f2bebf12bb026cae815f627722
Kavyan Nayak <kavyannayak104@gmail.com>
2026-08-07 18:39:13 +0530

    varibales use

--- Js/var.js
+++ Js/var.js
@@ -1,10 +1,39 @@
-var a = 10;
-var b = 20;
+// var a = 10;
+// var b = 20;
 
-var sum = a + b;
-var diff = a - b;
-var product = a * b;
+// var sum = a + b;
+// var diff = a - b;
+// var product = a * b;
 
-console.log("The Addition of 2 numbers :", sum);
-console.log("The Subtraction of 2 numbers :", diff);
-console.log("The Multiplication of 2 numbers :", product);
+// console.log("The Addition of 2 numbers :", sum);
+// console.log("The Subtraction of 2 numbers :", diff);
+// console.log("The Multiplication of 2 numbers :", product);
+
+
+// --------------------------------- Var ----------------------- 
+// re declaration 
+// var name = "Kavya";
+// var name = "Kavya2";
+// console.log("Name : " + name);
+// re initialization
+// var age = 20;
+// age = 21;
+// console.log("Age : " + age);
+
+
+// --------------------------------- let ----------------------- 
+
+// re declaration
+
+// let name = "Kavya";
+// // let name = "Kavya2";
+// name = "Kavya2";
+// console.log("Name : " + name);  
+
+
+// ------------------------ Const -----------------------
+
+const name = "Kavya";
+// const name = "Kavya2";
+name = "Kavya2";
+console.log("Name : " + name);
