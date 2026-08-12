78dabb28dbf414910c724fe783cc67292afcdc7e
Kavyan Nayak <kavyannayak104@gmail.com>
2026-08-12 18:47:50 +0530

    oprators

---
+++ Js/Basics/oprators.js
@@ -0,0 +1,42 @@
+// === op  -> equal the value and their type
+
+// a = 10;
+// b = "10";
+// b = 10;
+
+// document.write(a == b);
+// document.write(a === b);
+
+
+// <<=   -> left shift assignment
+
+// a = 10;
+// b = 2;
+
+//   10 << 2
+    // 1010 << 2
+    // 101000  -> 40
+
+// document.write(a <<= b);
+
+
+// >>= 
+
+// let a = 10;
+// let b = 2;
+
+// // 10 >> 2
+// // 1010 >> 2
+// //  0010  =  2
+
+//     // 1010
+//     // 0010
+
+// document.write(a >>= b);
+
+// !==   -> VALUE AND TYPE NOT EQUAL
+
+a = 10;
+b = "10";
+
+document.write(a !== b);
