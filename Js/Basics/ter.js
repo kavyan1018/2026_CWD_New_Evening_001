a31c26ff8197a060075ce55ba770d5d5a2b48a18
Kavyan Nayak <kavyannayak104@gmail.com>
2026-08-14 18:44:05 +0530

    task

--- Js/Basics/ter.js
+++ Js/Basics/ter.js
@@ -5,7 +5,19 @@
 *             condition ?  Ex1 : Ex2
 *
 * */
+//
+// let a = 18;
+// let num = a >= 18 ? "Adult" : "Child";
+// console.log(num);
 
-let a = 18;
-let num = a >= 18 ? "Adult" : "Child";
-console.log(num);
+
+
+// Positive, Zero, Neg
+
+a = prompt("Enter the Number :");
+
+let res = a > 0 ? "The Numebr is Positive !" :
+                 a < 0 ? "The Numebr is Negative !" :
+                 "The Numebr is Zero";
+
+console.log(res);
