abe4dbdd8fdc07946dddecc33997e1071ee7d6f7
Kavyan Nayak <kavyannayak104@gmail.com>
2026-08-19 18:34:44 +0530

    if else

---
+++ Js/if-else/if-elseDemo1.js
@@ -0,0 +1,30 @@
+/*
+
+    Syntax 
+
+        if(condition) {
+    
+            // true condition block of code
+
+        }
+        else{
+            // false condition block of code
+        }
+
+
+        
+
+
+*/
+
+
+// let age = 10;
+
+let age = prompt("Enter your age : ");
+
+if (age >= 18) {
+    console.log("You are eligible for vote !");
+}
+else {
+    console.log("You are not eligible for vote !");
+}
