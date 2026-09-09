533564c1554cc4a3c7d202d4045312a3125f830b
Kavyan Nayak <kavyannayak104@gmail.com>
2026-09-09 19:50:18 +0530

    Ternary op

---
+++ Js/BACK-UP/terop.js
@@ -0,0 +1,16 @@
+/*
+    its a sort version of if-else
+
+
+    syntax : 
+
+       //           True   false
+        condition ? exp 1 : exp2
+
+
+*/
+
+
+let a = prompt("Enter the Numebr Number to Check positive or Negative :");
+
+a > 0 ? console.log("The Number is Positive !") : console.log("The Number is Negative !");
