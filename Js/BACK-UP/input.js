bb1a7eb7d4390bf8316ad50f63136e356c0b684a
Kavyan Nayak <kavyannayak104@gmail.com>
2026-09-09 19:27:58 +0530

    user input

---
+++ Js/BACK-UP/input.js
@@ -0,0 +1,26 @@
+// user input 
+// let a = prompt("Enter the Number :");
+// console.log(a);
+
+
+// alert input 
+// alert("Hello World!!!!");
+
+// + concat 
+// alert("You Entered Value : " + a);
+
+
+
+// Defult values : String 
+
+// parseInt -> to conver the String into Numbers 
+// parsefloat ->  to conver the String into Decimal values
+// Numbers -> to conver the String into Numbers 
+
+
+let a = parseInt(prompt("Enter the First Number :"));
+let b = parseInt(prompt("Enter the Second Number :"));
+
+let sum = a + b;
+
+alert("The Sum of two Numbers : " + sum);
