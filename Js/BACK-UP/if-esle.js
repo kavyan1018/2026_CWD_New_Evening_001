95b16e251f596444968b1c85e5517f83f3a54207
Kavyan Nayak <kavyannayak104@gmail.com>
2026-09-23 19:35:50 +0530

    if else

---
+++ Js/BACK-UP/if-esle.js
@@ -0,0 +1,22 @@
+/*
+
+    if(condition){
+
+        // code 
+
+    }
+    else{
+        // code     
+    } 
+
+*/
+
+let a = prompt("Enter First Numebr :");
+let b = prompt("Enter Second Numebr :");
+
+if (a > b) {
+    console.log("A is Max !!!!");
+}
+else{
+    console.log("B is Max !!!!");
+}
