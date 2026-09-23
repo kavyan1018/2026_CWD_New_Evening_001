9e44008fc89c3b244ded38a083526bb93d525c36
Kavyan Nayak <kavyannayak104@gmail.com>
2026-09-23 19:52:54 +0530

    if else

--- Js/BACK-UP/if-esle.js
+++ Js/BACK-UP/if-esle.js
@@ -11,12 +11,30 @@
 
 */
 
-let a = prompt("Enter First Numebr :");
-let b = prompt("Enter Second Numebr :");
+// let a = prompt("Enter First Numebr :");
+// let b = prompt("Enter Second Numebr :");
 
-if (a > b) {
-    console.log("A is Max !!!!");
+// if (a > b) {
+//     console.log("A is Max !!!!");
+// }
+// else{
+//     console.log("B is Max !!!!");
+// }
+
+
+// else if 
+
+let a = Number(prompt("Enter First Numebr :"));
+let b = Number(prompt("Enter Second Numebr :"));
+let c = Number(prompt("Enter Third Numebr :"));
+
+
+if (a > b && a > c) {
+    console.log("A is Max....!!!");
+}
+else if(b > a && b > c){
+    console.log("B is Max....!!!");
 }
 else{
-    console.log("B is Max !!!!");
+    console.log("C is Max....!!!");
 }
